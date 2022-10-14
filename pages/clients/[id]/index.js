fb14bfac3926191d1c8bb116e2c78bbd54b78eea
Fernando Rios <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2022-10-13 20:53:52 -0500

    Add dynamic links to clients pages

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,11 +1,17 @@
 import { useRouter } from "next/router";
 
 const ClientProjectsPage = () => {
-  const { query } = useRouter();
+  const { query, push } = useRouter();
+
+  const loadProjectHandler = () => {
+    // load data...
+    push("/clients/max/projectA");
+  };
 
   return (
     <div>
       <h1>The Projects of {query.id}</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -11,9 +11,18 @@ const ClientsPage = () => {
       <h1>The Clients Page</h1>
       <ul>
         {clients.map((client) => {
-          return <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
-          </li>;
+          return (
+            <li key={client.id}>
+              <Link
+                href={{
+                  pathname: "/clients/[id]",
+                  query: { id: client.id },
+                }}
+              >
+                {client.name}
+              </Link>
+            </li>
+          );
         })}
       </ul>
     </div>
